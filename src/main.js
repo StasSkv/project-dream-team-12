45e9e6ccc400c38aa9ab03005528baa070e6fabb
StasSkv <stas000123@gmail.com>
2025-03-07 03:43:22 +0200

    start

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/benefits';
+import './js/projects';
+import './js/FAQ';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
