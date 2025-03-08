c4ebe0827e05c3be9359798ab371859a0e6bad05
Yuliia <lupin773311asd@gmail.com>
2025-03-08 22:52:08 +0000

    гнш

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,35 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const swiper = new Swiper(".container", {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+    const swiper = new Swiper(".swiper-container", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
-      },
-      on: {
-        slideChange: function () {
-          document.querySelector(".swiper-button-prev").disabled = this.isBeginning;
-          document.querySelector(".swiper-button-next").disabled = this.isEnd;
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        },
+        on: {
+            slideChange: function () {
+                document.querySelector(".swiper-button-prev").disabled = this.isBeginning;
+                document.querySelector(".swiper-button-next").disabled = this.isEnd;
+            }
         }
-      }
     });
-  });
+
+    // Перемещаем стрелки вниз под пагинацию
+    const navigationContainer = document.querySelector(".swiper-navigation");
+    const paginationContainer = document.querySelector(".swiper-pagination");
+
+    if (paginationContainer && navigationContainer) {
+        paginationContainer.insertAdjacentElement("afterend", navigationContainer);
+    }
+});
+
   
