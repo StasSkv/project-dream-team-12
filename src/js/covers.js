1324c4fdb33a4af9c5ce72106cd0a8753fca4d58
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-03-10 10:08:36 +0200

    initial commit

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,14 +1,6 @@
 
-    document.addEventListener("DOMContentLoaded", () => {
-     const covers = document.querySelector(".covers");
-     const observer = new IntersectionObserver(entries => {
-     entries.forEach(entry => {
-     if (entry.isIntersecting) {
-     covers.style.animation = "covers-left 10s linear infinite";
-     } else {
-     covers.style.animationPlayState = "paused";
-     }
-     });
-     }, { threshold: 0.1 });
-     observer.observe(covers);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    const coversList = document.querySelector(".covers__list");
+
+    coversList.style.animation = "covers-left 10s linear infinite";
+});
