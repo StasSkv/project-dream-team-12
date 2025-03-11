e5b09b49a52253e5d2197a0a9a38335983322b47
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-03-11 23:30:42 +0100

    update-footer

---
+++ src/js/work-together.js
@@ -0,0 +1,60 @@
+// import * as basicLightbox from 'basiclightbox';
+// import 'basiclightbox/dist/basicLightbox.min.css';
+
+// export function showModal(message) {
+//   const modal = basicLightbox.create(
+//     `
+//     <div class="basicLightbox__placeholder">
+//       <button class="modal-close">&times;</button>
+//       <h2>${message}</h2>
+//     </div>
+//   `,
+//     {
+//       onShow: instance => {
+//         const closeButton = instance.element().querySelector('.modal-close');
+//         closeButton.addEventListener('click', () => instance.close());
+//       },
+//     }
+//   );
+
+//   modal.show();
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const form = document.querySelector('.footer-form');
+
+//   if (!form) {
+//     console.error('Форма с id="contact-form" не найдена!');
+//     return;
+//   }
+
+//   form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     const email = form.querySelector('input[name="email"]').value.trim();
+//     const comment = form.querySelector('textarea[name="text"]').value.trim();
+
+//     if (!email || !comment) {
+//       console.error('Email или комментарий не заполнены!');
+//       showModal('Пожалуйста, заполните оба поля — Email и комментарий.');
+//       return;
+//     }
+
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           const data = JSON.parse(xhr.responseText);
+//           console.log('Успех:', data);
+//           showModal('Ваш запрос был успешно отправлен!');
+//           form.reset();
+//         } else {
+//           console.error('Ошибка:', xhr.status, xhr.statusText);
+//           showModal('Упс! Что-то пошло не так. Попробуйте позже.');
+//         }
+//       }
+//     };
+
+//     const requestData = JSON.stringify({ email, comment });
+//     xhr.send(requestData);
+//   });
+// });
