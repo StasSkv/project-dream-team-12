d15159aa9b819599b0a0dadb65ff4effc2e5ec25
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-03-11 17:06:54 +0200

    initial commit

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const lists = document.querySelectorAll('.covers__list');
 
-    // Функція для перемішування елементів
+   
     function shuffleItems(list) {
         const items = Array.from(list.querySelectorAll('.covers__item'));
     
@@ -11,27 +11,25 @@ document.addEventListener("DOMContentLoaded", () => {
             [items[i], items[j]] = [items[j], items[i]];
         }
     
-        // Очищуємо список і додаємо перемішані елементи
         list.innerHTML = '';
         items.forEach(item => list.appendChild(item));
     
-        // Додаємо анімацію після перемішування
+       
         list.style.animation = "covers-left 10s linear infinite";
     }
 
-    // Створюємо інтерсектор-спостерігач
+    // інтерсектор-спостерігач
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 // Якщо секція потрапляє в вьюпорт
-                shuffleItems(entry.target); // Перемішуємо елементи і додаємо анімацію
-                observer.unobserve(entry.target); // Прибираємо спостереження, якщо анімація вже почалася
+                shuffleItems(entry.target); // Перемішує елементи і додаэ анімацію
+                observer.unobserve(entry.target); // Прибирає спостереження, якщо анімація вже почалася
             }
         });
     }, {
-        threshold: 0.5 // Задаємо, коли спостерігач вважатиме, що елемент в межах вьюпорту (50%)
-    });
+        threshold: 0.5 
 
-    // Спостерігаємо за всіма секціями "Covers"
+    // Спостерігає за всіма секціями "Covers"
     lists.forEach(list => observer.observe(list));
 });
