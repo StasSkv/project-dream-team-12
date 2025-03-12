621cea880acbf99b0f793a7a754a489b5f6d8aad
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-03-12 13:03:41 +0100

    footer-update-new

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -2,7 +2,6 @@ import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
 let isModalOpen = false;
-
 function showModal(message) {
   if (isModalOpen) return;
 
@@ -15,14 +14,16 @@ function showModal(message) {
     {
       onShow: instance => {
         isModalOpen = true;
+        document.body.classList.add('no-scroll');
+
         const closeButton = instance.element().querySelector('.modal-close');
         closeButton.addEventListener('click', () => {
           instance.close();
-          isModalOpen = false;
         });
       },
       onClose: () => {
         isModalOpen = false;
+        document.body.classList.remove('no-scroll');
       },
     }
   );
@@ -34,22 +35,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('#contact-form');
   const emailInput = form.querySelector('input[name="email"]');
   const commentInput = form.querySelector('textarea[name="comment"]');
+  emailInput.value = localStorage.getItem('email') || '';
+  commentInput.value = localStorage.getItem('comment') || '';
 
-  // Функция сохранения в localStorage при вводе
   function saveToLocalStorage() {
     localStorage.setItem('email', emailInput.value.trim());
     localStorage.setItem('comment', commentInput.value.trim());
   }
 
-  // Восстанавливаем данные из localStorage при загрузке страницы
-  emailInput.value = localStorage.getItem('email') || '';
-  commentInput.value = localStorage.getItem('comment') || '';
-
-  // Добавляем обработчики ввода для сохранения данных в localStorage
   emailInput.addEventListener('input', saveToLocalStorage);
   commentInput.addEventListener('input', saveToLocalStorage);
 
-  // Обработчик отправки формы
   form.addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -62,10 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
       return;
     }
 
-    // Создаем объект для отправки
     const requestData = { email, comment };
-
-    // Отправка запроса через fetch
     fetch('https://portfolio-js.b.goit.study/api/requests', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -81,8 +74,6 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Успех:', data);
         showModal(data.message);
         form.reset();
-        localStorage.removeItem('email');
-        localStorage.removeItem('comment');
       })
       .catch(error => {
         console.error('Ошибка:', error.message);
